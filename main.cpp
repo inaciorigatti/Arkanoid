3e7db372ff505e868eecc6f7282e7fa0c976714d
Inácio Radin Rigatti <90224099+inaciorigatti@users.noreply.github.com>
2024-09-05 19:54:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,32 +11,33 @@ void SetColor(int color) {
 }
 
 void ResetColor() {
-    SetColor(15); // Cor padr„o (branco)
+    SetColor(15); // Cor padr√£o (branco)
 }
 
 int GetRandomColor() {
-    return 1 + rand() % 15; // Gera um n˙mero entre 1 e 15
+    return 1 + rand() % 15; // Gera um n√∫mero entre 1 e 15
 }
 
 int main()
 {
-    // Alerta: N„o modificar o trecho de cÛdigo, a seguir.
-    // InÌcio: Comandos para que o cursor n„o fique piscando na tela
+    // Alerta: N√£o modificar o trecho de c√≥digo, a seguir.
+    // In√≠cio: Comandos para que o cursor n√£o fique piscando na tela
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(out, &cursorInfo);
     cursorInfo.bVisible = false; // set the cursor visibility
     SetConsoleCursorInfo(out, &cursorInfo);
-    // Fim: Comandos para que o cursor n„o fique piscando na tela
-    // InÌcio: Comandos para reposicionar o cursor no inÌcio da tela
+    // Fim: Comandos para que o cursor n√£o fique piscando na tela
+    // In√≠cio: Comandos para reposicionar o cursor no in√≠cio da tela
     short int CX = 0, CY = 0;
     COORD coord;
     coord.X = CX;
     coord.Y = CY;
-    // Fim: Comandos para reposicionar o cursor no inÌcio da tela
-    // Alerta: N„o modificar o trecho de cÛdigo, acima.
+    // Fim: Comandos para reposicionar o cursor no in√≠cio da tela
+    // Alerta: N√£o modificar o trecho de c√≥digo, acima.
 
-     int TAM = 15;
+    int TAM = 15;
+
     int m[TAM][TAM] = {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         {1, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 1},
@@ -55,20 +56,48 @@ int main()
         {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
     };
 
-    // PosiÁ„o inicial do personagem no console
+    // Posi√ß√£o inicial do personagem no console
     int x = 12, y = 6;
-    // Vari·vel para tecla pressionada
+    // Vari√°vel para tecla pressionada
     char tecla;
 
-    srand(static_cast<unsigned>(time(0))); // Inicializa a semente para n˙meros aleatÛrios
-
+    srand(static_cast<unsigned>(time(0))); // Inicializa a semente para n√∫meros aleat√≥rios
     while (true) {
-        // Posiciona a escrita no inÌcio do console
+
+        // Posiciona a escrita no in√≠cio do console
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
         // Imprime o jogo: mapa e personagem.
         for (int i = 0; i < TAM; i++) {
             for (int j = 0; j < TAM; j++) {
+                
+                if (i == x && j == y){
+                     ResetColor();
+                    cout << char(110);
+                } else { 
+                    switch (m[i][j]){
+                         case 0:
+                            ResetColor();
+                            cout << " "; // caminho
+                            break;
+                    }
+                    
+                }cout << "\n";
+                  if (_kbhit()) {
+            tecla = getch();
+            switch (tecla) {
+                case 75: case 'a': // esquerda
+                    y--;
+                    break;
+                case 77: case 'd': // direita
+                    y++;
+                    break;
+            }
+        }
+                
+                
+                
+                
                 if (i == x && j == y) {
                     ResetColor();
                     cout << char(95); // personagem
@@ -83,8 +112,8 @@ int main()
                             cout << char(219); // bloco azul
                             break;
                         case 2:
-                            SetColor(GetRandomColor()); // Cor aleatÛria
-                            cout << char(219); // bloco em cor aleatÛria
+                            SetColor(GetRandomColor()); // Cor aleat√≥ria
+                            cout << char(219); // bloco em cor aleat√≥ria
                             break;
                         case 3:
                             SetColor(12); // Vermelho
